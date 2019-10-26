7149b7f544478089b99ef3b116808f0526cc8f5a
Anthony Conrad <8602418+parithon@users.noreply.github.com>
2019-10-26 05:18:36 -0700

    Using YALL for image lazy loading

--- FamPix.Web/wwwroot/js/site.js
+++ FamPix.Web/wwwroot/js/site.js
@@ -2,29 +2,4 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-window.addEventListener("load", (event) => {
-
-    const config = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0
-    };
-
-    let callback = (entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const src = entry.target.getAttribute("data-src");
-                entry.target.setAttribute("src", src);
-                observer.unobserve(entry.target);
-            }
-        });
-    };
-
-    let observer = new IntersectionObserver(callback, config);
-
-    const imgs = document.querySelectorAll('img[data-src]');
-    imgs.forEach(img => {
-        observer.observe(img);
-    });
-
-}, false);
+window.addEventListener("DOMContentLoaded", yall);
